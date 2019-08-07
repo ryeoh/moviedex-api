ce3502ae7b6372cb33e194e5f54f591d00994cbd
Rachel Oh <rachelyjo@gmail.com>
2019-08-06 18:25:25 -0700

    implemented 3 query parameters to filter through dataset

---
+++ server.js
@@ -0,0 +1,53 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+
+const movies = require('./movies-data-small.json');
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized request'});
+    }
+    // move to the next middleware
+    next();
+})
+
+function handleGetMovie (req, res) {
+    let response = movies;
+    const {genre, country, avg_vote} = req.query;
+
+    if (genre) {
+        response = movies.filter(movie => 
+            movie.genre.toLowerCase().includes(genre.toLowerCase())
+        )
+    } 
+    
+    if (country) {
+        response = response.filter(movie => 
+            movie.country.toLowerCase().includes(country.toLowerCase())
+        )
+    }
+
+    if(avg_vote) {
+        response = response.filter(movie => 
+            Number(movie.avg_vote) >= Number(avg_vote)
+        )
+    }
+
+    res.json(response);
+}
+
+app.get('/movie', handleGetMovie);
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+});
