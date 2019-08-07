9509c93d8ffa572118410375b89606f75e8dd094
Rachel Oh <rachelyjo@gmail.com>
2019-08-06 18:28:48 -0700

    add security measures

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
 
 const movies = require('./movies-data-small.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN;
